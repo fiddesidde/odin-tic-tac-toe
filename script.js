19cefd8bdd153aaa4a5fa5d5166c7adf4b73f43f
fiddesidde <fredrik.mellberg@gmail.com>
2021-03-03 21:52:20 +0100

    Add name entry view

--- script.js
+++ script.js
@@ -13,27 +13,81 @@ const Player = (name, symbol) => {
     return { getName, getSymbol, getScore, increaseScore, resetScore };
 };
 
-const Game = (() => {
-    const getPlayerNameFromUser = player => {
-        // let answer = '';
-        // do {
-        //     answer = prompt(`Enter name of ${player}!`);
-        // } while (!answer);
-        // return answer;
-        return player;
-    };
-    const p1 = Player(getPlayerNameFromUser('player 1'), 'X');
-    const p2 = Player(getPlayerNameFromUser('player 2'), 'O');
-    const players = [p1, p2];
+const DOM = (() => {
+    let names = [];
+    const container = document.querySelector('#container');
+    const body = document.querySelector('body');
+    const inputP1 = document.createElement('input');
+    const inputP2 = document.createElement('input');
 
-    let currentPlayer = p1;
+    const createSetUpView = () => {
+        container.style.display = 'none';
+
+        const setUpCont = document.createElement('div');
+        setUpCont.id = 'setUpCont';
+        const setUpCard = document.createElement('div');
+        setUpCard.className = 'card';
+        setUpCard.id = 'su-card';
+
+        const p1Div = document.createElement('div');
+        p1Div.id = 'winner';
+        p1Div.textContent = `Player 1, enter your name:`;
+        inputP1.type = 'text';
+
+        const p2Div = document.createElement('div');
+        p2Div.id = 'winner';
+        p2Div.textContent = `Player 2, enter your name:`;
+        inputP2.type = 'text';
+
+        const submitBtn = document.createElement('button');
+        submitBtn.textContent = 'GO!';
+        submitBtn.style.display = 'block';
+        submitBtn.addEventListener('click', () => {
+            Game.run([inputP1.value, inputP2.value]);
+            removeSetUpView();
+        });
+
+        setUpCard.appendChild(p1Div);
+        setUpCard.appendChild(inputP1);
+        setUpCard.appendChild(p2Div);
+        setUpCard.appendChild(inputP2);
+        setUpCard.appendChild(submitBtn);
+        setUpCont.appendChild(setUpCard);
+        body.appendChild(setUpCont);
+    };
+
+    const removeSetUpView = () => {
+        const cont = document.querySelector('#setUpCont');
+        container.style.display = 'flex';
+        body.removeChild(cont);
+    };
+
+    return { createSetUpView };
+})();
+
+const Game = (() => {
+    let p1 = '';
+    let p2 = '';
+    let currentPlayer = null;
+
+    const createPlayers = nameArray => {
+        p1 = Player(nameArray[0], 'X');
+        p2 = Player(nameArray[1], 'O');
+    };
+
+    const run = nameArray => {
+        createPlayers(nameArray);
+        currentPlayer = p1;
+        Display.setCurrentPlayerDiv();
+        Display.setPlayerNameDisplay();
+    };
+
+    const getPlayers = () => [p1, p2];
 
     const getCurrentPlayer = () => currentPlayer;
 
     const nextPlayer = () => {
         currentPlayer = currentPlayer === p1 ? p2 : p1;
-        // if (currentPlayer === p1) currentPlayer = p2;
-        // else currentPlayer = p1;
         Display.setCurrentPlayerDiv();
     };
 
@@ -57,7 +111,7 @@ const Game = (() => {
         nextPlayer();
     };
 
-    return { play, players, getCurrentPlayer, reset };
+    return { play, getPlayers, getCurrentPlayer, createPlayers, reset, run };
 })();
 
 const Display = (() => {
@@ -72,8 +126,10 @@ const Display = (() => {
     const winnerSpan = document.querySelector('#winner');
     const asides = document.querySelectorAll('.aside');
 
-    playernames[0].textContent = Game.players[0].getName();
-    playernames[1].textContent = Game.players[1].getName();
+    const setPlayerNameDisplay = () => {
+        playernames[0].textContent = Game.getPlayers()[0].getName();
+        playernames[1].textContent = Game.getPlayers()[1].getName();
+    };
 
     const showWinner = playerName => {
         board.style.display = 'none';
@@ -93,9 +149,8 @@ const Display = (() => {
     const setCurrentPlayerDiv = () => {
         curPlayerDiv.textContent = Game.getCurrentPlayer().getName();
     };
-    setCurrentPlayerDiv();
 
-    return { setCurrentPlayerDiv, showWinner, reset };
+    return { setCurrentPlayerDiv, showWinner, reset, setPlayerNameDisplay };
 })();
 
 const Gameboard = (() => {
@@ -165,3 +220,5 @@ const Gameboard = (() => {
 
     return { addSymbol, reset, checkWinState };
 })();
+
+DOM.createSetUpView();
